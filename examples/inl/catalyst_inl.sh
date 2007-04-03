01f5e28c3d44011f818ab5da66f14f5174b051f1
Philippe P Pebay <pppebay@sandia.gov>
2007-04-03 18:30:33 +0000

    SRS 4.15.6 #6760 An exemple script to run the INL model with MPICH on catalyst

---
+++ examples/inl/catalyst_inl.sh
@@ -0,0 +1,22 @@
+#!/bin/tcsh
+#PBS -N pcamal 
+#PBS -l nodes=10:ppn=2,walltime=1:00:00 
+
+# clean out module defaults (for now anyway)
+module purge
+
+# load in the mpi module needed.
+module load mpi/mpich2-1.0.5_p4_gcc-3.4.6
+
+#for mpich use:
+module load mpiexec
+set mpiexec_ops="-pernode"
+
+#for ompi their mpiexec.
+#mpiexec_ops='-bynode'
+
+# qsub 
+module load torque
+cd /home/pppebay/share-src/pcamal/examples/inl
+mpiexec -transform-hostname='s/$/-ib/g' pcamal_proto 612 reactor.pcamal.g meshed_reactor
+
